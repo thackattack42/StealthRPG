5619e1fee1fb9756b7d3416d64550cd01f6c27b1
Chase Thacker <thackerchase33@yahoo.com>
2024-05-07 01:52:18 -0400

    change to 5.1

--- Source/RPG_Tutorial_Series.Target.cs
+++ Source/RPG_Tutorial_Series.Target.cs
@@ -8,8 +8,8 @@ public class RPG_Tutorial_SeriesTarget : TargetRules
 	public RPG_Tutorial_SeriesTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.V1;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_1;
 		ExtraModuleNames.Add("RPG_Tutorial_Series");
 	}
 }
